d3e420532b2c352a8562776af13102f0e885cc17
Afnan Enayet <afnan.enayet@gmail.com>
2017-11-26 17:05:04 -0500

    renamed HTTP status code include file, updated documentation

---
+++ include/http_status_codes.h
@@ -0,0 +1,24 @@
+/* http_status.h    Afnan Enayet
+ *
+ * Defines constants defining HTTP status response codes.
+ *
+ * ---------------------------------------------------------------------------
+ * Codes:
+ *
+ * OK: 200
+ * Resource not found: 404
+ * Resource moved permanently: 303
+ * Resource moved temporarily: 302
+ * Server error: 500
+ */
+
+#ifndef _http_status_codes_h
+#define _http_status_codes_h
+
+#define HTTP_OK 200
+#define HTTP_NOT_FOUND 404
+#define HTTP_MOVED_PERMANENTLY 303
+#define HTTP_MOVED_TEMPORARILY 302
+#define HTTP_SERVER_ERROR 500
+
+#endif // _http_status_codes_h
