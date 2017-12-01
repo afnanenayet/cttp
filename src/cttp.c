e770f7ac9f6ce407d615393fcbba73509e1d4c3e
Afnan Enayet <afnan.enayet@gmail.com>
2017-11-30 23:16:52 -0500

    Updated command line args

--- src/cttp.c
+++ src/cttp.c
@@ -6,11 +6,18 @@
 
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <errno.h>
+#include "cttp_server.h"
+
+/****** constants ******/
+const int DEF_PORT = 8080;
 
 /****** function prototypes ******/
 
-bool arg_check(int argc, char *argv[]);
-void print_usage();
+static bool arg_check(int argc, char *argv[]);
+static void print_usage();
+static int str_to_port(char *port_str);
 
 /****** function definitions ******/
 
@@ -24,7 +31,13 @@ int main(int argc, char *argv[])
         print_usage();
         return 1;
     } else {
-        // TODO dispatch program
+        int port = str_to_port(argv[2]);
+
+        // with error, set to default port value
+        if (port == -1)
+            port = DEF_PORT;
+
+        cttp_server_run(port, NULL);
     }
 
     return 0;
@@ -34,21 +47,40 @@ int main(int argc, char *argv[])
  * function will return true. It will also print error messages as
  * necessary to stdout/stderr indicating which arguments are invalid
  */
-bool arg_check(int argc, char **argv)
+static bool arg_check(int argc, char **argv)
 {
-    if (argc < 2) {
+    if (argc < 3) {
         fprintf(stderr, "not enough arguments given\n");
         return false;
     } else {
-        printf("url: %s%s on port %s\n", argv[1], argv[2], argv[3]);
-        return true;
+        // see if we can convert port str to integer, if it is not a valid
+        // number, this is an error
+        return str_to_port(argv[2]) != -1;
     }
 }
 
 /* Prints instructions on how to use the program to STDOUT
 */
-void print_usage()
+static void print_usage()
 {
-    printf("usage: cttp [base url] [extra path (optional)] [port (optional, "
+    printf("usage: cttp [base path] [port (optional, "
             "default: 80)]\n");
+}
+
+/* given a string representing the port, returns the representation 
+ * of the port or returns -1 if the string is not parseable
+ */
+static int str_to_port(char *port_str)
+{
+    // Convert port to long
+    errno = 0;
+    char *endptr;
+    long port = strtol(port_str, &endptr, 10);
+
+    // If no error, return port. Otherwise return -1 as an error code
+    if (errno == 0) {
+        return (int) port;
+    } else {
+        return -1;
+    }
 }
