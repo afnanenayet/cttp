bc95523e30b9c227200e48e3388ff1496dd3c1a8
Afnan Enayet <afnan.enayet@gmail.com>
2017-12-01 13:51:51 -0500

    Updated commenting for cttp_server.h

--- include/cttp_server.h
+++ include/cttp_server.h
@@ -7,9 +7,15 @@
 #ifndef _cttp_server_h
 #define _cttp_server_h
 
+/****** public function prototypes ******/
+
 /* Starts the CTTP server and runs all necessary dispatch functions to respond
  * to requests and dispatch files
+ *
+ * params:
+ * - port: the port to serve from
+ * - root: the name of the root directory to serve files from
  */
-int cttp_server_start(const char *addr, int port, const char *root);
+int cttp_server_run(int port, const char *root);
 
 #endif // _cttp_server_h
