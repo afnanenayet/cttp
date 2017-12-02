2aa1cd6f18cee204a22312912dcc19b39dfd428c
Afnan Enayet <afnan.enayet@gmail.com>
2017-12-02 13:34:44 -0500

    changed arg check to return a particular error code instead of a bool

--- src/cttp.c
+++ src/cttp.c
@@ -15,24 +15,24 @@
 
 const int DEF_PORT = 8080;
 
-/****** function prototypes ******/
+/****** private function prototypes ******/
 
-static bool arg_check(int argc, char *argv[]);
+static int arg_check(int argc, char *argv[]);
 static void print_usage();
 static int str_to_port(char *port_str);
 
-/****** function definitions ******/
+/****** public function definitions ******/
 
 int main(int argc, char *argv[])
 {
-    bool run = arg_check(argc, argv);
-    int ret_code = 0;  // return code from server
+    // return code from argument check, cttp server
+    int ret_code = arg_check(argc, argv);
 
     // If arguments are invalid, print proper usage and exit
     // otherwise start main program
-    if (!run) {
+    if (ret_code != 0) {
         print_usage();
-        return 1;
+        return ret_code;
     } else {
         int port = DEF_PORT;
 
@@ -48,18 +48,26 @@ int main(int argc, char *argv[])
     return ret_code;
 }
 
+/****** private function definitions ******/
+
 /* Checks the arguments to see if they are valid or not. If they are, the
  * function will return true. It will also print error messages as
  * necessary to stdout/stderr indicating which arguments are invalid
+ *
+ * Will return an error code that corresponds to the argument given.
+ * - 0: arguments ok
+ * - 1: not enough arguments supplied
+ * - 2: invalid port number
+ * - 3: invalid base path supplied
  */
-static bool arg_check(int argc, char **argv)
+static int arg_check(int argc, char **argv)
 {
     if (argc < 2) {
         fprintf(stderr, "not enough arguments given\n");
-        return false;
+        return 1;
     } else if (!is_valid_dir(argv[1])) {
         fprintf(stderr, "supplied base path is not a valid directory\n");
-        return false;
+        return 3;
     } else if (argc >= 3) {
         // see if we can convert port str to integer, if it is not a valid
         // number, this is an error
@@ -67,9 +75,9 @@ static bool arg_check(int argc, char **argv)
 
         if (port == -1) {
             fprintf(stderr, "invalid port provided\n");
-            return false;
+            return 2;
         } else
-            return true;
+            return 0;
     } else {
         return true;
     }
