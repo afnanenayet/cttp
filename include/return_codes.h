0ef03bc0086fd8c7e3e8614b314a819a377788b1
Afnan Enayet <afnan.enayet@gmail.com>
2017-12-03 17:55:14 -0500

    Defined return codes header file Macros defining return codes that the program can return

---
+++ include/return_codes.h
@@ -0,0 +1,14 @@
+/* return_codes.h
+ *
+ * Macros defining the return codes that the program can return
+ */
+
+#ifndef _RETURN_CODES_H
+#define _RETURN_CODES_H
+
+#define RET_OK (0) // all systems ok
+#define RET_ARG_ERROR (1) // generic argument error
+#define RET_NET_ERROR (2) // networking error
+#define RET_UNK_ERROR (3) // error unknown, probably SIGTERM
+
+#endif // _RETURN_CODES_H
