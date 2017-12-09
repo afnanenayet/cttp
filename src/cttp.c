ca9278dd4daeb16fdf87fdce92418a07a1485487
Afnan Enayet <afnan.enayet@gmail.com>
2017-12-09 01:13:28 -0500

    Removed arg functions from cttp

--- src/cttp.c
+++ src/cttp.c
@@ -8,81 +8,42 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <errno.h>
+#include <unistd.h> // getopt
+#include "return_codes.h"
 #include "cttp_server.h"
 #include "file_dispatch.h"
+#include "arg_util.h"
 
 /****** constants ******/
 
 const int DEF_PORT = 8080;
 
 /****** private function prototypes ******/
-
-static int arg_check(int argc, char *argv[]);
 static void print_usage();
-static int str_to_port(char *port_str);
 
 /****** public function definitions ******/
 
 int main(int argc, char *argv[])
 {
-    // return code from argument check, cttp server
-    int ret_code = arg_check(argc, argv);
+    // arguments will live here so that they can be passed around as a
+    // bundle as necessary
+    cttp_args args;
+    int ret_code;
+    extract_args(argc, argv, &args);
 
     // If arguments are invalid, print proper usage and exit
     // otherwise start main program
-    if (ret_code != 0) {
-        print_usage();
+    if (verify_args(&args)) {
+        ret_code = cttp_server_run(args.port, args.dir);
         return ret_code;
     } else {
-        int port = DEF_PORT;
-
-        // set port to supplied port per user's request
-        if (argc > 2)
-            port = str_to_port(argv[2]);
-
-        // with error, set to default port value
-        if (port == -1)
-            port = DEF_PORT;
-        ret_code = cttp_server_run(port, argv[1]);
+        print_usage();
+        return RET_ARG_ERROR;
     }
     return ret_code;
 }
 
 /****** private function definitions ******/
-
-/* Checks the arguments to see if they are valid or not. If they are, the
- * function will return true. It will also print error messages as
- * necessary to stdout/stderr indicating which arguments are invalid
- *
- * Will return an error code that corresponds to the argument given.
- * - 0: arguments ok
- * - 1: not enough arguments supplied
- * - 2: invalid port number
- * - 3: invalid base path supplied
- */
-static int arg_check(int argc, char **argv)
-{
-    if (argc < 2) {
-        fprintf(stderr, "not enough arguments given\n");
-        return 1;
-    } else if (!is_valid_dir(argv[1])) {
-        // if only one arg is supplied it has to be a valid dir
-        fprintf(stderr, "supplied base path is not a valid directory\n");
-        return 3;
-    } else if (argc >= 3) {
-        // see if we can convert port str to integer, if it is not a valid
-        // number, this is an error
-        int port = str_to_port(argv[2]);
-
-        if (port == -1) {
-            fprintf(stderr, "invalid port provided\n");
-            return 2;
-        } else
-            return 0;
-    } else {
-        return 0;
-    }
-}
 
 /* Prints instructions on how to use the program to STDOUT
 */
@@ -90,22 +51,4 @@ static void print_usage()
 {
     printf("usage: cttp [base path] [port (optional, "
             "default: 80)]\n");
-}
-
-/* given a string representing the port, returns the representation
- * of the port or returns -1 if the string is not parseable
- */
-static int str_to_port(char *port_str)
-{
-    // Convert port to long
-    errno = 0;
-    char *endptr;
-    long port = strtol(port_str, &endptr, 10);
-
-    // If no error, return port. Otherwise return -1 as an error code
-    if (errno == 0) {
-        return (int) port;
-    } else {
-        return -1;
-    }
 }
