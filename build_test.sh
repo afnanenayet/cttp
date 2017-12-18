52996792904612d301a11709f75faa551e0967e0
Afnan Enayet <afnan.enayet@gmail.com>
2017-12-18 15:32:08 -0500

    Added -j flag to make test so tests will be run in parallel Speeds up testing a bit

--- build_test.sh
+++ build_test.sh
@@ -1,4 +1,4 @@
 # build_test.sh
 # builds cttp binary and library and uses ctest to execute unit tests
 
-./build.sh && env CTEST_OUTPUT_ON_FAILURE=1 make test
+./build.sh && env CTEST_OUTPUT_ON_FAILURE=1 make -j test
