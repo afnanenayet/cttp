92d5bc3471a016a54dc7ee42b20710ed5a0085c6
Afnan Enayet <afnan.enayet@gmail.com>
2017-12-18 15:35:51 -0500

    Created unit tests for mime_types.h Completed unit tests (100% codecov for mime_types.h)

--- test/src/mime_types_test.c
+++ test/src/mime_types_test.c
@@ -15,28 +15,87 @@
 
 #include "mime_types.h"
 
-// TODO...all of these tests lol
 void ext_to_mime_test_valid_mimes()
 {
+    char *inputs[] = {
+        "html",
+        "svg",
+        "gif",
+        "txt",
+        NULL,
+    };
+
+    char *expected[] = {
+        "text/html",
+        "image/svg+xml",
+        "image/gif",
+        "text/plain",
+        NULL,
+    };
+
+    for (int i = 0; inputs[i] != NULL; i++) {
+        assert_string_equal(ext_to_mime(inputs[i]), expected[i]);
+    }
 }
 
 void mime_to_ext_test_valid_exts()
 {
+    char *expected[] = {
+        "html",
+        "svg",
+        "gif",
+        "txt",
+        NULL,
+    };
+
+    char *inputs[] = {
+        "text/html",
+        "image/svg+xml",
+        "image/gif",
+        "text/plain",
+        NULL,
+    };
+
+    for (int i = 0; inputs[i] != NULL; i++) {
+        assert_string_equal(mime_to_ext(inputs[i]), expected[i]);
+    }
 }
 
 void ext_to_mime_test_invalid_exts()
 {
+    const int num_bad_inputs = 2;
+    char *inputs[] = {
+        "asdf;lkasdjf",
+        NULL,
+    };
+
+    // Invalid inputs should all return NULL
+    for (int i = 0; i < num_bad_inputs; i++) {
+        assert_null(ext_to_mime(inputs[i]));
+    }
 }
 
 void mime_to_ext_test_invalid_mimes()
 {
+    const int num_bad_inputs = 2;
+    char *inputs[] = {
+        "asdf;lkasdjf",
+        NULL,
+    };
+
+    for (int i = 0; i < num_bad_inputs; i++) {
+        assert_null(mime_to_ext(inputs[i]));
+    }
 }
 
 int main(void)
 {
     const struct CMUnitTest tests[] =
     {
-        cmocka_unit_test(some_fn_ptr_here);
+        cmocka_unit_test(ext_to_mime_test_valid_mimes),
+        cmocka_unit_test(mime_to_ext_test_invalid_mimes),
+        cmocka_unit_test(mime_to_ext_test_valid_exts),
+        cmocka_unit_test(ext_to_mime_test_invalid_exts),
     };
 
     return cmocka_run_group_tests(tests, NULL, NULL);
